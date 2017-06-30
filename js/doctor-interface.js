7548a5b656adc220f8ebe85063561a883e86117a
Arianna Chau <arianna@ariannas-mbp.lan>
2017-06-30 10:08:57 -0700

    start doctorInfo prototype to grab info from apio

---
+++ js/doctor-interface.js
@@ -0,0 +1,5 @@
+var doctorInfo = require('./../js/doctor.js').doctorModule;
+
+$(document).ready(function() {
+  var doctor = new doctorInfo();
+})
---
+++ js/doctor.js
@@ -0,0 +1,10 @@
+var apiKey = require('./../.env').apiKey;
+
+function doctorInfo() {
+}
+
+doctorInfo.prototype.docInfo = function(condition) {
+  var doctors = [];
+  $('#output').text("");
+  doctorInfo
+}
