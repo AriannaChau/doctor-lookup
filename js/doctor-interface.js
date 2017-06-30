bd65b4c0c924ec05ae169de333d362603d8a8b3c
Arianna Chau <arianna@ariannas-mbp.lan>
2017-06-30 15:21:33 -0700

    Add filler text to fill in white space, the filler text will hide when user clicks Find Doctor button

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,14 @@
 var doctorInfo = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
+  $('ul').hide();
   var doctors = new doctorInfo();
   $('#find').click(function(event) {
     event.preventDefault();
     var conditions = $('#search-symptoms').val();
     doctor = doctors.docInfo(conditions);
     $('#search-symptoms').val("");
-    $('#search').hide();
-    $('#clear').show();
+    $('#filler').hide();
+    $('ul').show();
   })
 });
