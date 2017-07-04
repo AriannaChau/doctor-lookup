e4feba8526790258575f19015b6770155d51b3b8
Arianna Chau <arianna@ariannas-mbp.lan>
2017-07-03 22:18:40 -0700

    Move front end logic into interface file. Convert constructor functions to PascalCase

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,14 +1,43 @@
-var doctorInfo = require('./../js/doctor.js').doctorModule;
+var DoctorInfo = require('./../js/doctor.js').doctorModule;
+
+
+var displayDoc = function(doctors) {
+  if (doctors.length === 0) {
+    $('#result-li').append("<h4>No results. Try getting a little more specific!");
+  } else {
+    doctors.forEach(function(doc) {
+        if (doc.title != undefined) {
+          doc.title = doc.title;
+        } else {
+          doc.title = "n/a";
+        }
+
+        if (doc.bio != "") {
+          doc.bio = doc.bio;
+        } else {
+          doc.bio = "No bio available.";
+        }
+
+      $('#result-li').append(
+        "<li><h3>" + doc.firstName + " " + doc.lastName + ", " + doc.title + "</h3><p>" + doc.bio + "</p></li>"
+      );
+    });
+  }
+};
 
 $(document).ready(function() {
   $('ul').hide();
-  var doctors = new doctorInfo();
+  var doctors = new DoctorInfo();
   $('#find').click(function(event) {
     event.preventDefault();
     var conditions = $('#search-symptoms').val();
-    doctor = doctors.docInfo(conditions);
+    var doctorArray = doctors.docInfo(conditions);
+    setTimeout(function() {
+      displayDoc(doctorArray);
+    }, 1500);
     $('#search-symptoms').val("");
     $('#filler').hide();
     $('ul').show();
   })
+
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,9 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
-function doctorInfo() {
+function DoctorInfo() {
 }
 
-doctorInfo.prototype.docInfo = function(condition) {
+DoctorInfo.prototype.docInfo = function(condition) {
   var doctors = [];
   $('#result-li').text("");
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ condition+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
@@ -16,34 +16,11 @@ doctorInfo.prototype.docInfo = function(condition) {
         bio: doctor.profile.bio,
       });
     });
-
-    doctors.forEach(function(doctor) {
-      if (doctor.title != undefined) {
-        doctor.title = doctor.title;
-      } else {
-        doctor.title = "n/a";
-      }
-    });
-
-    doctors.forEach(function(doctor) {
-      if (doctor.bio != "") {
-        doctor.bio = doctor.bio;
-      } else {
-        doctor.bio = "No bio available.";
-      }
-    });
-    if (doctors.length === 0) {
-      $('#result-li').append("<h4>No results. Try getting a little more specific!");
-    } else {
-      doctors.forEach(function(doc) {
-        $('#result-li').append(
-          "<li><h3>" + doc.firstName + " " + doc.lastName + ", " + doc.title + "</h3><p>" + doc.bio + "</p></li>"
-        );
-      });
-    }
   })
   .fail(function(error){
+    console.log("Getting doctors failed")
   });
+  return doctors;
 };
 
-exports.doctorModule = doctorInfo;
+exports.doctorModule = DoctorInfo;
